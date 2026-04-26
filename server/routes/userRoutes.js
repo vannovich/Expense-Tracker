84dd5a4b18622010eb92867ad42f1e928feff3da
vannovich <awontuvannovich@gmail.com>
2026-04-27 00:24:34 +0100

    Set up auth Middleware and userController

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,15 @@
 import { Router } from "express";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import {
+  changePassword,
+  getUser,
+  updateUser,
+} from "../controller/userController.js";
 
 const router = Router();
 
+router.get("/", authMiddleware, getUser);
+router.put("/change-password", authMiddleware, changePassword);
+router.put("/", authMiddleware, updateUser);
 
 export default router;
